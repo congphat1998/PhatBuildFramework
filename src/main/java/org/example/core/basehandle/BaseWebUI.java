c1cff17d4f2c9137b5679e18516ce1299284fadb
you <you@example.com>
2023-06-05 22:10:20 +0700

    add sth

--- src/main/java/org/example/core/basehandle/BaseWebUI.java
+++ src/main/java/org/example/core/basehandle/BaseWebUI.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.asserts.Assertion;
+
 
 import java.time.Duration;
 import java.util.List;
